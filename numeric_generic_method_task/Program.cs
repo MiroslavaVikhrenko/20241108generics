3f423d81d59c6b59b9dafc8b78bb8987069bc563
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-11-08 17:09:41 -0700

    Add a single linked list

---
+++ numeric_generic_method_task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeric_generic_method_task
+{
+    internal class Program
+    {
+        //Разработчику необходимо написать обобщенный метод, который может работать с любой коллекцией,
+        //но он хочет ограничить этот метод только коллекциями, содержащими элементы числового типа.
+        //Он не знает, как этого добиться, не ограничивая метод.
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            //var result = Sum(array);
+
+            //Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        //static T Sum<T>(IEnumerable<T> values) where T : INumber<T>
+        //{
+        //    T sum = default;
+
+        //    foreach (var value in values)
+        //    {
+        //        sum += value;
+        //    }
+
+        //    return sum;
+        //}
+    }
+}
